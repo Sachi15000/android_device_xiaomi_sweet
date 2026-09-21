c0c633196f3ac9633e1c8780b83f5df7ba619a1c
Sachi15000 <akunkerenbos9@gmail.com>
2026-09-21 20:37:58 +0800

    sweet: vendorsetup.sh add some colors

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,72 @@
 #!/bin/bash
 
-# Common Tree
-git clone https://github.com/PixelOS-Devices/android_device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
-git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_xiaomi_sm6150-common.git -b seventeen vendor/xiaomi/sm6150-common
+set -o pipefail
 
-# Vendor Sweet & MiuiCamera
-git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_xiaomi_sweet.git -b seventeen vendor/xiaomi/sweet
-git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_miuicamera-sweet.git -b seventeen vendor/miuicamera-sweet
+LOG_FILE="clone_$(date +%Y%m%d_%H%M%S).log"
+STEP=0
 
-# Kernel Sm6150
-git clone https://github.com/PixelOS-Devices/android_kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+GREEN="\033[1;32m"
+RED="\033[1;31m"
+CYAN="\033[1;36m"
+YELLOW="\033[1;33m"
+RESET="\033[0m"
 
-# Hardware Xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+log_step() {
+    STEP=$((STEP + 1))
+    echo -ne "${CYAN}[$STEP]${RESET} $1 ... "
+}
 
-# Dolby OnePlus Vendor
-git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_oneplus_dolby.git -b seventeen vendor/oneplus/dolby
+log_ok() {
+    echo -e "${GREEN}done${RESET}"
+}
 
-# Lunaris
-git clone https://github.com/sweet-stuffs/android_packages_apps_LunarisDolby.git packages/apps/LunarisDolby
+log_fail() {
+    echo -e "${RED}failed${RESET} (see $LOG_FILE)"
+}
+
+log_skip() {
+    echo -e "${YELLOW}already exists, skipped${RESET}"
+}
+
+clone() {
+    local label="$1"
+    local url="$2"
+    local dest="$3"
+    local branch="$4"
+
+    log_step "$label"
+
+    if [ -d "$dest" ]; then
+        log_skip
+        return
+    fi
+
+    if [ -n "$branch" ]; then
+        git clone -b "$branch" "$url" "$dest" >> "$LOG_FILE" 2>&1
+    else
+        git clone "$url" "$dest" >> "$LOG_FILE" 2>&1
+    fi
+
+    if [ $? -eq 0 ]; then
+        log_ok
+    else
+        log_fail
+    fi
+}
+
+echo -e "${CYAN}=== Starting clone process ===${RESET}"
+echo "Full log: $LOG_FILE"
+echo
+
+clone "Clone common tree"        "https://github.com/PixelOS-Devices/android_device_xiaomi_sm6150-common.git" "device/xiaomi/sm6150-common"
+clone "Clone vendor common"      "https://gitlab.com/NoPrincessHere/proprietary_vendor_xiaomi_sm6150-common.git" "vendor/xiaomi/sm6150-common" "seventeen"
+clone "Clone vendor sweet"       "https://gitlab.com/NoPrincessHere/proprietary_vendor_xiaomi_sweet.git" "vendor/xiaomi/sweet" "seventeen"
+clone "Clone MiuiCamera sweet"   "https://gitlab.com/NoPrincessHere/proprietary_vendor_miuicamera-sweet.git" "vendor/miuicamera-sweet" "seventeen"
+clone "Clone kernel sm6150"      "https://github.com/PixelOS-Devices/android_kernel_xiaomi_sm6150.git" "kernel/xiaomi/sm6150"
+clone "Clone hardware xiaomi"    "https://github.com/LineageOS/android_hardware_xiaomi.git" "hardware/xiaomi"
+clone "Clone dolby oneplus"      "https://gitlab.com/NoPrincessHere/proprietary_vendor_oneplus_dolby.git" "vendor/oneplus/dolby" "seventeen"
+clone "Clone LunarisDolby"       "https://github.com/sweet-stuffs/android_packages_apps_LunarisDolby.git" "packages/apps/LunarisDolby"
+
+echo
+echo -e "${CYAN}=== Done ($STEP repos processed) ===${RESET}"
 
